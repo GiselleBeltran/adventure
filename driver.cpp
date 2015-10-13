84f0f8a54ea67f31a2664cfc5dde51b66c9baeb3
GiselleBeltran <gisellembeltran@student.hartnell.edu>
2015-10-13 13:54:20 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -5,25 +5,78 @@
 // Github Labwork
 
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
-char menu();
+char menu(char answer);
 int numberOfPeople();
 double discount(int people);
-double climbing(int people);
-double scuba(int people);
-double skyDive(int people, int discountPrice);
-double spelunk(int people);
+double climbing(int people, double discountPrice);
+double scuba(int people, double discountPrice);
+double skyDive(int people, double discountPrice);
+double spelunk(int people, double discountPrice);
 
 int main()
 {
-
-
+    double discountPrice;
+    char answer;
+    int people;
+    
+cout << "Welcome to High Adventure Travel Agency\n" << endl;
+    cout << "There are four vacation packages available: \n" << endl;
+    cout << "Devil's Courthouse Adventure Weekend: \n"
+         << "Rate: \n"
+         << "\tBase Charge:" << "\t \t  \t   $350 per person\n" 
+         << "\tClimbing Instruction:" << "\t \t   $100 per person\n"  
+         << "\tEquipment Rental:" << "\t \t   $40/day per person" << endl;
+   cout << "\nScuba Bahama: \n"
+         << "Rate: \n"
+         << "\tBase Charge:" << "\t \t \t   $1000 per person\n" 
+         << "\tScuba Instruction:" << "\t \t   $100 per person\n" << endl;
+    cout << "Sky Dive Colorado: \n"
+         << "Rate: \n"
+         << "\tBase Charge:" << "\t \t \t   $700 per person\n"
+         << "\tLodging at Wilderness Lodge:" << "\t   $65/day per person\n"  
+         << "\tLodging at Luxury Inn:" << "\t \t   $120/day per person" << endl;
+    cout << "\nBarron Cliff Spelunk: \n"
+         << "Rate: \n"
+         << "\tBase Charge:" << "\t \t \t   $700 per person\n"  
+         << "\tEquipment Rental:" << "\t \t   $40/day per person" << endl;
+    cout << "\nPlease select a package that you'd like: " << endl; 
+         cout << "Your choices are: \n"
+              << "\nClimbing package: press 'A' or 'a'\n"
+              << "\nScuba package: press 'B' or 'b'\n"
+              << "\nSky Dive package: press 'C' or 'c'\n"
+              << "\nHiking package: press 'D' or 'd'\n" << endl;
+         cin  >> answer;
+         menu(answer);
+         
+         
+         
   return 0;
 }
 // Need to use independent variables in the int main(). Set int people = numberOfPeople() and double discountPrice = discount(int people). Make sure to execute functions within menu() instead of int main().
-char menu(){
+char menu(char answer){
+	answer = tolower(answer);
+	int people = numberOfPeople();
+    double discountPrice = discount(people);
+	
+	switch (answer)
+	{
+		case 'a': climbing(people, discountPrice);
+		break;
+		
+		case 'b': scuba(people, discountPrice);
+		break;
+		
+		case 'c': skyDive(people, discountPrice);
+		break;
+		
+		case 'd': spelunk(people, discountPrice);
+		break;
+	}
+	
     
 }
 int numberOfPeople()
@@ -42,7 +95,7 @@ double discount(int people)
         discountPrice = (0.0);
     return discountPrice;
 }
-double climbing(int people){
+double climbing(int people, double discountPrice){
     double priceTotal;
 	string climbingInstruction, equipRental;
 	
@@ -58,11 +111,12 @@ double climbing(int people){
         priceTotal += (people*40)*(3);
     }
     if (people >= 5){
-    	priceTotal -= (priceTotal*(discount(people)));
+    	priceTotal -= (priceTotal*(discountPrice));
     }
+    cout << "Total: $"<<priceTotal << endl;
     return priceTotal;
 }
-double scuba(int people){
+double scuba(int people, double discountPrice){
 	double priceTotal;
 	string scubaInstruction;
 	
@@ -71,14 +125,15 @@ double scuba(int people){
 	
 	priceTotal = (people * 1000.00);
 	if ((scubaInstruction == "Yes") || (scubaInstruction == "yes")){
-		priceTotal += (people * 100);
+		priceTotal += (people * 100.00);
 	}
 	if (people >= 5){
-	    priceTotal += (priceTotal*(discount(people)));
+	    priceTotal += (priceTotal*(discountPrice));
 	}
+	cout << "Total: $"<<priceTotal << endl;
 	return priceTotal;
 }
-double skyDive(int people, int discountPrice)
+double skyDive(int people, double discountPrice)
 {
     char choice;
     double price;
@@ -95,22 +150,23 @@ double skyDive(int people, int discountPrice)
         price = ((700 - (700 * discountPrice)) * people) + (120 * people); 
     else
         price = (700 - (700 * discountPrice))* people;
-    cout << price << endl;
+    cout << "Total: $" << price << endl;
     return price;
 }
-double spelunk(int people){
-    double priceTotal
+double spelunk(int people, double discountPrice){
+    double priceTotal;
     string equipRental;
     
     cout << "Will you need to rent equipment?\n";
     cin >> equipRental;
     
-    priceTotal = (people * 700);
+    priceTotal = (people * 700.00);
     if ((equipRental == "Yes") || (equipRental == "yes")){
         priceTotal += ((people * 40) * 8);
     }
     if (people >= 5){
-        priceTotal += (priceTotal*(discount(people)));
+        priceTotal += (priceTotal*(discountPrice));
     }
+    cout << "Total: $"<<priceTotal << endl;
     return priceTotal;
 }
